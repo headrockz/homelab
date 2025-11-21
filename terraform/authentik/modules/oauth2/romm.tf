44526528b53486da9609451943e0d7a8dbffe46e
Asafe Felipe <azapphrz@gmail.com>
2025-11-21 11:41:20 -0300

    feat: create romm oauth config

---
+++ terraform/authentik/modules/oauth2/romm.tf
@@ -0,0 +1,33 @@
+resource "random_password" "romm" {
+  length  = 40
+  special = false
+}
+
+resource "authentik_provider_oauth2" "romm" {
+  name               = "RomM"
+  client_id          = resource.random_password.romm.result
+  authorization_flow = var.default_authorization_flow
+  invalidation_flow  = var.default_invalidation_flow
+  signing_key        = var.default_self_singned
+    property_mappings = [
+    var.oauth_profile_id,
+    var.oauth_openid_id,
+    var.oauth_email_id
+  ]
+  allowed_redirect_uris = [
+    {
+      matching_mode = "regex",
+      url           = "https://romm.${var.domain}/api/oauth/openid",
+    }
+  ]
+}
+
+resource "authentik_application" "romm" {
+  name              = "RomM"
+  slug              = "romm"
+  group             = ""
+  protocol_provider = authentik_provider_oauth2.romm.id
+  meta_launch_url   = "https://romm.${var.domain}/"
+  meta_icon         = "/media/public/application-icons/romm.png"
+  open_in_new_tab   = true
+}
