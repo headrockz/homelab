94de531be7abbfebbd0060747a3edbf69262ec02
Asafe Felipe <azapphrz@gmail.com>
2025-11-24 15:01:45 -0300

    feat: create download images for lxc

--- terraform/proxmox/main.tf
+++ terraform/proxmox/main.tf
@@ -1,6 +1,7 @@
 module "acme" {
   source = "./modules/acme"
 
+  proxmox_node_name    = var.proxmox_node_name
   cloudflare_email     = var.cloudflare_email
   cloudflare_api_token = var.cloudflare_api_token
 }
@@ -8,12 +9,21 @@ module "acme" {
 module "dns" {
   source = "./modules/dns"
 
-  dns = var.dns
+  proxmox_node_name = var.proxmox_node_name
+  dns               = var.dns
 }
 
 module "metrics" {
   source = "./modules/metrics"
 
-  influxdb_host  = var.influxdb_host
-  influxdb_token = var.influxdb_token
+  proxmox_node_name = var.proxmox_node_name
+  influxdb_host     = var.influxdb_host
+  influxdb_token    = var.influxdb_token
+}
+
+module "lxc" {
+  source = "./modules/lxc"
+
+  proxmox_node_name = var.proxmox_node_name
+  dns               = var.dns
 }
