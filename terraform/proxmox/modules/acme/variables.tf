a6f2ab9354c5e451f15b6321ad2ea2e9830731e1
Asafe Felipe <azapphrz@gmail.com>
2025-11-24 14:36:18 -0300

    feat: add proxmox hostname variable

--- terraform/proxmox/modules/acme/variables.tf
+++ terraform/proxmox/modules/acme/variables.tf
@@ -1,3 +1,10 @@
+variable "proxmox_node_name" {
+  description = "Proxmox Node Name"
+  type        = string
+  sensitive   = false
+  default     = "homeone"
+}
+
 variable "cloudflare_email" {
   description = "Cloudflare API email"
   type        = string
